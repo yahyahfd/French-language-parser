b5a07bc2471f48fd66090d95fac86f21f2a6af37
Antoine KHOW <anto.kh@hotmail.fr>
2021-04-07 10:14:26 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-<<<<<<< parser.py
 #!/usr/bin/env python3
 # dans une phrase simple, les mots sont séparés par des espaces: On cherche à séparer ces termes et à les mettre dans une liste
 # le premier séparateur est l'espace, le deuxième est la virgule et autres ponctuations (collés aux termes), le troisième est l'apostrophe comme dans "l'espace".
