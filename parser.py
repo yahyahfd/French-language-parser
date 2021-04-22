1d0380830c8d19d224f5ffb3bb6a9adf91f672c0
Antoine KHOW <anto.kh@hotmail.fr>
2021-04-22 21:09:14 +0200

    Amelioration de la recherche de verbe avec le dico de verbe

--- parser.py
+++ parser.py
@@ -22,14 +22,16 @@ def init_dico(tab):
         dico[mot] = "nom"
     return dico
 
-def comp_terminaison(dico, treatment, mot):
-    if treatment in mot and mot[len(mot)-len(treatment):len(mot):1] == treatment:
-        dico[mot] = "verbe"
+def comp_dico(dico, treatment, mot):
+    #if treatment in mot and mot[len(mot)-len(treatment):len(mot):1] == treatment:
+    if treatment[0] == mot:
+        dico[mot] = 'verbe' + ' ' + treatment[-1]
+    
     return dico
 
 def parse_dict(tab):
     dico = init_dico(tab)
-    fd = open('conjugaison.txt')
+    fd = open('lefff-verbs.txt')
 
     #itération du fichier conjugaison (terminaisons des verbes)
     for line in fd:
@@ -38,8 +40,8 @@ def parse_dict(tab):
         else:
             #itération du dict (phrase en input splitted via ' ')
             for mot in tab:
-                treatment = line.rstrip()
-                dico = comp_terminaison(dico, treatment, mot)
+                treatment = parse_ligne(line.rstrip())
+                dico = comp_dico(dico, treatment, mot)
                 '''nopunc = mot.strip(punc)
                 dico = comp_terminaison(dico, treatment, nopunc)'''
 
