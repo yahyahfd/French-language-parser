f036e831cfcaf4bc2150025f5327c12939f340cf
Antoine KHOW <anto.kh@hotmail.fr>
2021-04-28 18:58:03 +0200

    Ajout d'une fonction pour avoir une meilleure piste du sujet

--- interpreteur.py
+++ interpreteur.py
@@ -8,18 +8,68 @@ def comp_dico(dico, treatment, mot):
     
     return dico
 
-def parse_dict(tab):
-    dico = tab
-    fd = open('lefff-verbs.txt')
+def search_verbe(tab):
+    dico_key = tab
+    fd_dico = open('lefff-verbs.txt')
 
     #itération du fichier conjugaison (terminaisons des verbes)
-    for line in fd:
+    for line in fd_dico:
         if '#' in line or '-' in line or line == '':
             continue
         else:
             #itération du dict (phrase en input splitted via ' ')
             for mot in tab:
                 treatment = parser.parse_ligne(line.rstrip())
-                dico = comp_dico(dico, treatment, mot)
+                dico_key = comp_dico(dico_key, treatment, mot)
                 '''nopunc = mot.strip(punc)
                 dico = comp_terminaison(dico, treatment, nopunc)'''
+    
+    return dico_key
+
+def append_premiere(piste_sujet, v):
+    if '1' in v:
+        if 's' in v:
+            piste_sujet.append('je')
+        elif 'p' in v:
+            piste_sujet.append('nous')
+    
+    return piste_sujet
+
+def append_deuxieme(piste_sujet, v):
+    if '2' in v:
+        if 's' in v:
+            piste_sujet.append('tu')
+        elif 'p' in v:
+            piste_sujet.append('vous')
+    
+    return piste_sujet
+
+def append_troisieme(piste_sujet, v):
+    if '3' in v:
+        if 's' in v:
+            piste_sujet.append('il')
+            piste_sujet.append('elle')
+            piste_sujet.append('on')
+            piste_sujet.append(piste_sujet[0])
+            piste_sujet[0] = '3s'
+        elif 'p' in v:
+            piste_sujet.append('ils')
+            piste_sujet.append('elles')
+    
+    return piste_sujet
+
+
+def traitement_forme_verbale(dico_key):
+    piste_sujet = []
+    for (k, v) in dico_key.items():
+        if v == 'nom':
+            continue
+        
+        piste_sujet = append_premiere(piste_sujet, v)
+
+        piste_sujet = append_deuxieme(piste_sujet, v)
+
+        piste_sujet = append_troisieme(piste_sujet, v)
+
+    return piste_sujet
+        
